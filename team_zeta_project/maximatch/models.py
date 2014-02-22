3951dd78157d960feccfbbc05c7b49a8618565de
Ubuntu <ubuntu@ip-172-31-32-253.eu-west-1.compute.internal>
2014-02-22 23:20:40 +0000

    Updated models, added population script, issues with DateTimeField

--- team_zeta_project/maximatch/models.py
+++ team_zeta_project/maximatch/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# Researcher - Participant - Experiment - Application
+
 class Researcher(models.Model):
     name = models.CharField(max_length=128)
     email = models.EmailField(blank=False, unique=True)
@@ -14,22 +15,29 @@ class Participant(models.Model):
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
+        ('O', 'Other'), # just being politically correct
     )
     NATIONALITY_CHOICES = (
-        ('United Kingdom', 'United Kingdom'),
+        ('GB', 'British'),
+	('BR', 'Brazilian'),
+	('BG', 'Bularian'),
         )
     FIRST_LANGUAGE_CHOICES = (
-        ('United Kingdom', 'United Kingdom'),
+        ('English', 'English'),
+	('Portuguese', 'Portuguese'),
     )
     EDUCATION_LEVEL_CHOICES = (
         ('High school', 'High school'),
+	('Bachalor', 'Bachalor'),
     )
     name = models.CharField(max_length=128)
     email = models.EmailField(blank=False, unique=True)
     password = models.CharField(max_length=128, blank=False)
     matriculation_id = models.CharField(max_length=7)
-    nationality = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    date_of_birth = models.DateField()
+    nationality = models.CharField(max_length=2, choices=NATIONALITY_CHOICES)
+    date_of_birth = models.DateField(null=True)
+    # Ethically participants should not disclose phone numbers?
+    # Maybe we can have just one field for it?
     mobile_number = models.CharField(max_length=11)
     telephone_number = models.CharField(max_length=11)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
@@ -44,6 +52,9 @@ class Experiment(models.Model):
         ('Closed', 'Closed'),
         ('Open to applicants', 'Open to applicants'),
     )
+    # Also I feel status, start_date, published are overlapping?
+    title = models.CharField(max_length=128, blank=False)
+    #can't we have one description and shorten it on an app level?
     short_description = models.CharField(max_length=45, blank=False)
     description = models.CharField(max_length=1024)
     participants_needed = models.IntegerField()
@@ -53,13 +64,15 @@ class Experiment(models.Model):
     payment_cash = models.DecimalField(decimal_places=2,max_digits=7)
     payment_credit = models.IntegerField()
     payment_other = models.CharField(max_length=128)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    published = models.DateTimeField()
+    start_date = models.DateField(null=True, blank=False)
+    end_date = models.DateField(null=True, blank=False)
+    # Having problems populating this field :/ I tried this format 2014-12-22 15:12:13
+    #published = models.DateTimeField(null=True, blank=False)
     researcher = models.ForeignKey(Researcher)
 
     def __unicode__(self):
         return self.short_description
+
 class Application(models.Model):
     STATUS_CHOICES = (
         ('Email sent', 'Email sent'),
---
+++ team_zeta_project/populate_maximatch.py
@@ -0,0 +1,72 @@
+import os
+
+def populate():
+	# Adding a Researcher
+	mickey = add_researcher(name = 'Mickey', 
+							email = 'pashov.m@gmail.com', 
+							password = '123456', 
+							matriculation_id = '1005139')
+
+	# Adding a Participant
+	bruno = add_participant(name = 'Bruno', 
+							email = 'bruno@developer.com', 
+							password = 'coolbeans',
+     						matriculation_id = '2105470', 
+     						nationality = 'BR', 
+     						date_of_birth = '1990-05-22',
+	 						mobile_number = '08457642359', 
+	 						telephone_number = '5555555555', 
+	 						gender = 'M', 
+	 						first_language = 'Portuguese', 
+	 						education_level = 'Bachalor')
+
+	# Adding an Experiment
+	test = add_experiment(title = 'Test Experiment', 
+								short_description = 'This is a test experiment.', 
+								description = 'It will be used as a way of populating our database. Don\'t forget that models and the population scrip are tightly coupled',
+     							participants_needed = 20, 
+     							status = 'Closed', 
+     							location = '58 Hillhead Street, Waiting Room',
+      							duration = '24 hours', 
+      							payment_cash = 25.00, 
+      							payment_credit = 2, 
+      							payment_other = 'Ice cream',
+      							start_date = 2014-03-22, 
+      							end_date = 2014-04-01, 
+      							published = , 
+      							researcher = mickey)
+	# Add application
+	app = add_application(participant = bruno, experiment = test, status = 'Email sent')
+
+# 
+def add_researcher(name, email, password, matriculation_id):
+    r = Researcher.objects.get_or_create(name = name, email = email, password = password, matriculation_id = matriculation_id)[0]
+    return r
+
+def add_participant(name, email, password, matriculation_id, nationality, date_of_birth,
+	 mobile_number, telephone_number, gender, first_language, education_level):
+
+    p = Participant.objects.get_or_create(name = name, email = email, password = password,
+     matriculation_id = matriculation_id, nationality = nationality, date_of_birth = date_of_birth,
+	 mobile_number = mobile_number, telephone_number = telephone_number, gender = gender, first_language = first_language, education_level = education_level)[0]
+    return p
+
+def add_experiment(title, short_description, description, participants_needed, status,
+	 location, duration, payment_cash, payment_credit, payment_other, start_date, end_date, published, researcher):
+
+    e = Experiment.objects.get_or_create(title = title, short_description = short_description, description = description,
+     participants_needed = participants_needed, status = status, location = location,
+      duration = duration, payment_cash = payment_cash, payment_credit = payment_credit, payment_other = payment_other,
+      start_date = start_date, end_date = end_date, published = published, researcher = researcher)[0]
+    return e
+
+def add_application(participant, experiment, status):
+    a = Application.objects.get_or_create(participant = participant, experiment = experiment, status = status)[0]
+    return a
+
+# Start execution here!
+if __name__ == '__main__':
+    print "Starting MaxiMatch population script..."
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_zeta_project.settings')
+    from maximatch.models import Researcher, Participant, Experiment, Application
+    populate()
