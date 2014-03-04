15511b6499bfc40194269f858c2da976337b6762
Eduardo <durds.edu@gmail.com>
2014-03-04 14:42:13 +0000

    authentication app and user model added, changed participant and researcher model

--- team_zeta_project/maximatch/models.py
+++ team_zeta_project/maximatch/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Researcher - Participant - Experiment - Application
 
 class Researcher(models.Model):
-    name = models.CharField(max_length=128)
-    email = models.EmailField(blank=False, unique=True)
-    password = models.CharField(max_length=128, blank=False)
+    # the commented lines are now part of the Users model, from the authentication app.
+
+    #name = models.CharField(max_length=128)
+    #email = models.EmailField(blank=False, unique=True)
+    #password = models.CharField(max_length=128, blank=False)
     matriculation_id = models.CharField(max_length=7)
+    user = models.OneToOneField(User)
 
     def __unicode__(self):
-        return self.name
+        return self.user.username
 
 class Participant(models.Model):
     GENDER_CHOICES = (
@@ -30,31 +34,28 @@ class Participant(models.Model):
         ('High school', 'High school'),
 	('Bachelor', 'Bachelor'),
     )
-    name = models.CharField(max_length=128)
-    email = models.EmailField(blank=False, unique=True)
-    password = models.CharField(max_length=128, blank=False)
+    #name = models.CharField(max_length=128)
+    #email = models.EmailField(blank=False, unique=True)
+    #password = models.CharField(max_length=128, blank=False)
     matriculation_id = models.CharField(max_length=7)
     nationality = models.CharField(max_length=2, choices=NATIONALITY_CHOICES)
     date_of_birth = models.DateField(null=True)
-    # Ethically participants should not disclose phone numbers?
-    # Maybe we can have just one field for it?
     mobile_number = models.CharField(max_length=11)
     telephone_number = models.CharField(max_length=11)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     first_language = models.CharField(max_length=45, choices=FIRST_LANGUAGE_CHOICES)
     education_level = models.CharField(max_length=45, choices=EDUCATION_LEVEL_CHOICES)
+    user = models.OneToOneField(User)
 
     def __unicode__(self):
-        return self.name
+        return self.user.username
 
 class Experiment(models.Model):
     STATUS_CHOICES = (
         ('Closed', 'Closed'),
         ('Open to applicants', 'Open to applicants'),
     )
-    # Also I feel status, start_date, published are overlapping?
     title = models.CharField(max_length=128, blank=False)
-    #can't we have one description and shorten it on an app level?
     short_description = models.CharField(max_length=45, blank=False)
     description = models.CharField(max_length=1024)
     participants_needed = models.IntegerField()
@@ -85,4 +86,4 @@ class Application(models.Model):
     status = models.CharField(max_length=25, choices=STATUS_CHOICES)
 
     def __unicode__(self):
-        return self.participant.name+'-'+self.experiment.short_description
+        return self.participant.user.username+'-'+self.experiment.short_description
--- team_zeta_project/maximatch/views.py
+++ team_zeta_project/maximatch/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from maximatch.models import Experiment
+from django.contrib.auth.models import User
 
 # Create your views here.
 from maximatch.forms import ExperimentForm
@@ -79,3 +80,45 @@ def add_experiment(request):
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
     return render_to_response('maximatch/add_experiment.html', {'form': form}, context)
+
+
+def register(request):
+    context = RequestContext(request)
+
+    registered = False
+
+    # If it's a HTTP POST, we're interested in processing form data.
+    if request.method == 'POST':
+        # Attempt to grab information from the raw form information.
+        # Note that we make use of both UserForm and UserProfileForm.
+        user_form = User(data=request.POST)
+
+        # If the two forms are valid...
+        if user_form.is_valid():
+            # Save the user's form data to the database.
+            user = user_form.save()
+
+            # Now we hash the password with the set_password method.
+            # Once hashed, we can update the user object.
+            user.set_password(user.password)
+            user.save()
+
+            # Update our variable to tell the template registration was successful.
+            registered = True
+
+        # Invalid form or forms - mistakes or something else?
+        # Print problems to the terminal.
+        # They'll also be shown to the user.
+        else:
+            print user_form.errors
+
+    # Not a HTTP POST, so we render our form using two ModelForm instances.
+    # These forms will be blank, ready for user input.
+    else:
+        user_form = User()
+
+    # Render the template depending on the context.
+    return render_to_response(
+            'maximatch/register.html',
+            {'user_form': user_form, 'registered': registered},
+            context)
